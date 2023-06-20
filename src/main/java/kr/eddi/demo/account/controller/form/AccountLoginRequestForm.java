13b4a748a79f1ed959d702f38a899f241e96b3db
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-06-20 17:15:53 +0900

    사용자가 로그인을 할 수 있게 합니다[STOCK-CHAT-SPRING - 2]

---
+++ src/main/java/kr/eddi/demo/account/controller/form/AccountLoginRequestForm.java
@@ -0,0 +1,11 @@
+package kr.eddi.demo.account.controller.form;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+@Getter
+@RequiredArgsConstructor
+public class AccountLoginRequestForm {
+
+    private String email;
+    private String password;
+}
