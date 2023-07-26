ee5d2bff7748fd9f8879b1a9fb626e73de027236
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-26 17:23:58 +0900

    feat: [유경우] allowedOrigins를 안보이게 숨깁니다[SPB-43]

--- src/main/java/kr/eddi/demo/config/CorsConfig.java
+++ src/main/java/kr/eddi/demo/config/CorsConfig.java
@@ -1,19 +1,19 @@
 package kr.eddi.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
-    @Autowired
-    private AllowedOriginsConfig allowedOriginsConfig;
+    final private AllowedOriginsConfig allowedOriginsConfig;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOriginsConfig.getAllowedOrigins().toArray(String[]::new)) // 변경
+                .allowedOrigins(allowedOriginsConfig.getAllowedOrigins().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)
