724e956f754698056a4e0c0f0f0e03ca4605ff22
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-27 18:42:01 +0900

    feat: [유경우] 스케쥴러나 이니셜라이저의 사용을 알 수 있게합니다[SPB-45]

--- src/main/java/kr/eddi/demo/initializer/StockInitializer.java
+++ src/main/java/kr/eddi/demo/initializer/StockInitializer.java
@@ -1,11 +1,13 @@
 package kr.eddi.demo.initializer;
 
 import kr.eddi.demo.domain.stock.service.StockService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class StockInitializer {
 
 //    @Bean
@@ -23,8 +25,11 @@ public class StockInitializer {
     @Bean
     public CommandLineRunner onStartUp(StockService stockService) {
         return args -> {
+            log.info("Stock Initializer Start");
             stockService.save();
+            log.info("Opinion Initializer Start");
             stockService.saveOpinion();
+            log.info("OCVA Save Initializer Start");
             stockService.saveOCVAData();
         };
     }
--- src/main/java/kr/eddi/demo/scheduler/StockScheduler.java
+++ src/main/java/kr/eddi/demo/scheduler/StockScheduler.java
@@ -1,9 +1,10 @@
 package kr.eddi.demo.scheduler;
 
 import kr.eddi.demo.domain.stock.service.StockService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 public class StockScheduler {
     private final StockService stockService;
@@ -14,11 +15,14 @@ public class StockScheduler {
 
     @Scheduled(fixedDelay = 10800000)
     public void scheduleSaveOpinion() {
+        log.info("Opinion Scheduler Start");
+
         stockService.saveOpinion();
     }
 
     @Scheduled(fixedDelay = 10800000)
     public void scheduleSaveOCVAData() {
+        log.info("OCVA Save Scheduler Start");
         stockService.saveOCVAData();
     }
 }
